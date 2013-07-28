1b55e91e7518e8d67a2bdbad9452de8874049a2f
Peter Graham <peterghm@gmail.com>
2013-07-27 23:26:34 -0700

    Add uglify grunt task and generate uglified file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,18 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        mangle: true,
+        compress: true,
+        report: 'gzip'
+      },
+      dist: {
+        files: {
+          'src/shrimp.min.js': ['src/shrimp.js']
+        }
+      }
+    },
     jshint: {
       files: ['gruntfile.js', 'src/shrimp.js', 'test/*.js'],
       options: {
@@ -14,12 +26,13 @@ module.exports = function(grunt) {
     },
     watch: {
       files: ['<%= jshint.files %>'],
-      tasks: ['jshint']
+      tasks: ['jshint', 'uglify']
     }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint', 'uglify']);
 };
